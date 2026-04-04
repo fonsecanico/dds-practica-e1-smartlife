d3c5f10b4d08fee75bcd3dc753d201a1be938d75
Nicolás Fonseca Giudici <nicofonseca47@gmail.com>
2026-04-04 14:22:41 -0300

    springboot commit

---
+++ src/main/java/ar/edu/utn/ba/ddsi/practica/dds_practica_e1_smartlife/DdsPracticaE1SmartlifeApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.practica.dds_practica_e1_smartlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdsPracticaE1SmartlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdsPracticaE1SmartlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/ba/ddsi/practica/dds_practica_e1_smartlife/DdsPracticaE1SmartlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.practica.dds_practica_e1_smartlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsPracticaE1SmartlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
